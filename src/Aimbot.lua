520c10a142b32b824e8e1421cc5b8db4457ba999
progangstare-dot <progangstare@gmail.com>
2026-03-17 04:37:40 -0500

    Update Aimbot.lua

--- src/Aimbot.lua
+++ src/Aimbot.lua
@@ -103,7 +103,7 @@ getgenv().ExunysDeveloperAimbot = {
 		OffsetToMoveDirection = true,
 		OffsetIncrement = 1,
 
-		Sensitivity = .5, -- Animation length (in seconds) before fully locking onto target
+		Sensitivity = 0.5, -- Animation length (in seconds) before fully locking onto target
 		Sensitivity2 = 3.5, -- mousemoverel Sensitivity
 
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
